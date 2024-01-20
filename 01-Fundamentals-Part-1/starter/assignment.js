30b6cd62136221cec34b216db75fe8e2fccad1d6
ngille188 <ngille@utilimarc.com>
2024-01-20 08:49:54 -0600

    first assignment

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,11 @@
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country = 'The country of Green and Gold'
+let continent = 'Super Bowl'
+let population = 53
+
+console.log(country)
+console.log(continent)
+console.log(population)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log(js)
+
+let goat = 'Aaron Rodgers'
+let notGoat = 'Thomas Brady'
+console.log(goat)
