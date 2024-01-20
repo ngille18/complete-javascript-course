5f03ae91aca69c738ec88199c2365406370810b4
ngille188 <ngille@utilimarc.com>
2024-01-20 09:58:33 -0600

    fun

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,117 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
-console.log(js)
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+// console.log(js);
 
-let goat = 'Aaron Rodgers'
-let notGoat = 'Thomas Brady'
-console.log(goat)
+// let goat = 'Aaron Rodgers';
+// let notGoat = 'Thomas Brady';
+// console.log(goat);
+
+// let javaScriptisOkay = true;
+// console.log(javaScriptisOkay);
+
+// console.log(typeof true);
+// console.log(javaScriptisOkay);
+// console.log(typeof 23);
+// console.log(typeof 'Nate');
+
+// javaScriptisOkay = 'maybe';
+// console.log(typeof javaScriptisOkay);
+
+// let foo;
+// console.log(foo);
+// console.log(typeof foo);
+
+// foo = 'bar'
+// console.log(typeof foo);
+
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const packerAreGreat = true;
+// packerAreGreat = false;
+
+// console.log(1 > 2);
+
+// const ageNate = 2099 - 1999;
+// console.log('ageNate', ageNate);
+// console.log(ageNate * 2, ageNate / 2, 2 ** 3);
+
+// const firstName = 'Paula';
+// const lastName = 'Abdule';
+// console.log(firstName + ' ' + lastName);
+
+// let vikingsSuperBowls = 0 + 0;
+// console.log(vikingsSuperBowls);
+
+// vikingsSuperBowls += 0;
+// console.log(vikingsSuperBowls);
+
+// vikingsSuperBowls++;
+// vikingsSuperBowls--;
+// console.log(vikingsSuperBowls)
+
+// let packersSuperBowls = 4;
+// console.log('Packers have won more SBs than the Vikings', packersSuperBowls > vikingsSuperBowls);
+
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 92;
+// let jonHeight = 1.95;
+
+// let markBMI = markWeight / markHeight ** 2;
+// let johnBMI = johnWeight / jonHeight ** 2;
+
+// let markHigherBMI = markBMI > johnBMI;
+// console.log(markBMI, johnBMI, markHigherBMI);
+
+// markWeight = 95;
+// markHeight = 1.88;
+// johnWeight = 85;
+// jonHeight = 1.76;
+
+// markBMI = markWeight / (markHeight * markHeight);
+// johnBMI = johnWeight / jonHeight ** 2;
+
+// markHigherBMI = markBMI > johnBMI;
+// console.log(markBMI, johnBMI, markHigherBMI);
+
+// const firstName = 'Nate'
+// const birthYear = 1998
+
+// const me = "I'm " + firstName + " and I wasn't born in " + birthYear + "."
+// console.log(me)
+
+// const meLiteral = `I'm ${firstName} and I wasn't born in ${birthYear}.`
+// console.log(meLiteral)
+
+// console.log(`string
+// multi line
+// string
+// foo
+// bar
+// `)
+
+
+// const age = 1;
+
+// if (age >= 18) {
+//     console.log(`You can start driving.`);
+// }
+// else {
+//     const yearsLeft = 18 - age
+//     console.log(`You are too young. Wait ${yearsLeft} years... or don't I'm not in charge of you.`)
+// }
+
+// const birthYear = 1991;
+// let century;
+
+// if (birthYear <= 2000) {
+//     century = 20;
+// }
+// else {
+//     century = 21;
+// }
+
+// console.log(`You were born in the ${century} century.`)
