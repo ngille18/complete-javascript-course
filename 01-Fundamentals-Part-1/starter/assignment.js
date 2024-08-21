3f5758a9dcf94a645187b24063a0684c822cf880
ngille188 <ngille@utilimarc.com>
2024-08-20 20:02:56 -0500

    so fun

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -2,6 +2,7 @@
 
 // Log their values to the console.
 
+/*
 let country = 'The country of Green and Gold'
 let continent = 'Super Bowl'
 let population = 53
@@ -9,3 +10,47 @@ let population = 53
 console.log(country)
 console.log(continent)
 console.log(population)
+
+
+let foo;
+let bar = null;
+
+console.log("foo: " + foo)
+console.log("bar: " + bar)
+console.log(foo == bar)
+console.log(foo === bar)
+console.log(typeof foo)
+console.log(typeof bar)
+
+
+let firstName = "Greg";
+const newThing = `Hi, my name is ${firstName} whats up ${100 - 3 / 8}??`;
+console.log(newThing)
+
+console.log(
+    `one
+    two
+    three`
+)
+
+const age = 19;
+
+if(age >= 18){
+    console.log(`Nice, you're ${age} years old and can drive 🚘`)
+} else{
+    console.log(`nooooo driving`)
+}
+
+
+const inputYear = '1991';
+console.log(typeof inputYear, typeof Number(inputYear))
+
+console.log(Number('Foo'), typeof NaN)
+
+console.log(String(100))
+
+console.log(Boolean(1), Boolean(" "), Boolean({}))
+console.log(Boolean(0), Boolean(""), Boolean(NaN), Boolean(null), Boolean(undefined))
+*/
+
+const age = 18;
