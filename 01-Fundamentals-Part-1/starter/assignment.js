537088db4ff02e0a2d4d01803f74f7a952cce349
ngille188 <ngille@utilimarc.com>
2024-09-02 14:52:11 -0500

    fun stuff

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -51,6 +51,64 @@ console.log(String(100))
 
 console.log(Boolean(1), Boolean(" "), Boolean({}))
 console.log(Boolean(0), Boolean(""), Boolean(NaN), Boolean(null), Boolean(undefined))
-*/
 
 const age = 18;
+
+if (age === 18) console.log('You are old')
+
+const num = prompt("enter a number")
+
+
+if (Number(num) === 12) {
+    console.log('Go Aaron Rodgers')
+} else if (Number(num) === 10) {
+    console.log('Go Jordan Love')
+}
+else {
+    console.log('Go Pack Go')
+}
+
+if (Number(num) !== 12) {
+    console.log('Boooooooo')
+}
+
+
+const canDrive = false;
+const hasGoodVis = true;
+
+console.log(canDrive && hasGoodVis);
+console.log(canDrive || hasGoodVis);
+console.log(!canDrive || hasGoodVis);
+
+*/
+
+// const day = 'friday';
+
+// switch(day) {
+//     case 'monday':
+//         console.log('happy monday :(')
+//         // more code here
+//         break
+//     case 'tuesday':
+//         console.log('its not monday')
+//         break
+//     case 'wednesday':
+//     case 'thursday':
+//     case 'friday':
+//         console.log('party time')
+//         break
+//     case 'sunday':
+//         console.log('footballllllllll!!!!!!!!!!')
+//         break
+//     default:
+//         console.log('dude what?')
+// }
+
+
+const age = 9;
+// age >= 18 ? console.log('enjoy the draft') : console.log('no draft no problems');
+
+const baz = age >= 18 ? 'foo' : 'bar';
+console.log(baz)
+
+console.log(`Hello ${age >= 18 ? 'foo' : 'bar'}`);
